4bc3da73122b8f9ebf0feb0b50eec8cbb9febea6
Shreyanshi-Vashistha <Shreyanshi-Vashistha@users.noreply.github.com>
2023-12-25 15:24:47 -0500

    Added cards on home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,74 @@
-import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from "react-native";
+import {
+  Card,
+  CardImage,
+  CardTitle,
+  CardContent,
+  CardAction,
+  CardButton,
+} from "react-native-material-cards"; // Import your card components here
 import { signOut } from "firebase/auth";
 import { auth } from "../config/config";
+import Bike from "../assets/images/bike.jpg";
+import Car from "../assets/images/car.jpg";
+import Fan from "../assets/images/fan.jpg";
 
-export default function HomeScreen() {
+const data = [
+  { id: "1", thing: "Car", price: "$20000", sub: "Clean Title", Img: Car },
+  { id: "2", thing: "Bike", price: "$10000", sub: "Brand New", Img: Bike },
+  { id: "3", thing: "Fan", price: "$200", sub: "Pre-owned", Img: Fan },
+  // Add more items as needed
+];
+
+const HomeScreen = () => {
   const handleLogout = async () => {
     await signOut(auth);
   };
-  return (
-    <SafeAreaView className="flex-1 flex-row justify center items-center">
-      <Text className="text-lg">Home Page -</Text>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text className="text-lg">Logout </Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+
+  const renderCard = ({ item }) => (
+    <Card>
+      <CardImage
+        // source={item.Img}
+        title={item.thing}
+        style={styles.cardImage}
+      />
+      <CardTitle title={item.price} subtitle={`${item.sub}`} />
+      {/* <CardContent text={`Content for ${item.title}`} /> */}
+      <CardAction separator={true} inColumn={false}>
+        <CardButton onPress={() => {}} title="Add to Cart" color="blue" />
+        <CardButton onPress={() => {}} title="Buy Now" color="blue" />
+      </CardAction>
+    </Card>
   );
-}
+
+  return (
+    <View>
+      <FlatList
+        data={data}
+        renderItem={renderCard}
+        keyExtractor={(item) => item.id}
+      />
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardImage: {
+    // Add styles for CardImage here
+    width: "100%",
+    height: "20%", // Adjust the height as needed
+    // or "contain" or "stretch"
+  },
+});
+
+export default HomeScreen;
